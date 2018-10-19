4804f169cd12b6c058855e48cb831c1635b97d70
Peter Jakobs <pjakobs@mobileme.de>
2018-10-19 20:08:46 +0200

    minor changes to install

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
+echo "### installing dependencies"
 apt install python-setuptools python-psutil python-pil
 pip install Adafruit-SSD1306 Adafruit-BBIO
+echo "### copying files"
 cp status.py /usr/bin/
 cp status.service /etc/systemd/system
+echo "### reloading systemd"
+systemctl daemon-reload
+echo "### starting service"
+sysetmctl start status
